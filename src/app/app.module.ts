613b5eefcd68f0191d1466c50046fb8124ea8540
Vlado-VG <whiteskymage@gmail.com>
2022-08-10 18:13:37 +0300

    Added all videos to the lessons/streaming page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,23 +7,23 @@ import { NavbarComponent } from './navigation_component/navbar.component';
 import { HomeComponent } from './home_component/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { LessonsComponent } from './lessons/lessons.component';
-<<<<<<< Updated upstream
-=======
 import { StreamsComponent } from './streams/streams.component';
 import { ArticlesComponent } from './articles/articles.component';
 
->>>>>>> Stashed changes
+
 
 
 @NgModule({
   declarations: [
-    AppComponent, 
-<<<<<<< Updated upstream
-    NavbarComponent, HomeComponent, AboutUsComponent, LessonsComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent, 
+    AboutUsComponent, 
+    LessonsComponent, 
+    StreamsComponent, 
+    ArticlesComponent
   ],
-=======
-    NavbarComponent, HomeComponent, AboutUsComponent, LessonsComponent, StreamsComponent, ArticlesComponent],
->>>>>>> Stashed changes
+
   imports: [
     BrowserModule,
     AppRoutingModule
